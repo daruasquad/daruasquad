31b72deaa92f1c687d5fa7b0ced36fa94aa6e97f
Bruno Nogueira de Oliveira <ti.brunonogueira@gmail.com>
2014-06-28 20:22:32 -0300

    Dados de conexão com Banco de dados através do arquivo properties funcionando.
    	modificado: configs/bd-config.properties
    	modificado: src/main/java/br/ufg/pw/utilitarios/PropertiesManipulador.java

--- src/main/java/br/ufg/pw/dao/LocalDao.java
+++ src/main/java/br/ufg/pw/dao/LocalDao.java
@@ -1,15 +1,23 @@
 package br.ufg.pw.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.List;
 
 import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
 
 import br.ufg.pw.entidades.Local;
+import br.ufg.pw.utilitarios.JdbcUtil;
 
 @ManagedBean
 @ApplicationScoped
 public class LocalDao {
+	
+	Connection conn;
+	PreparedStatement pstmt;
+	ResultSet rs;
 	
 	public void inserir(Local local) {
 		//Implementar a inserção no Banco de Dados
@@ -18,5 +26,32 @@ public class LocalDao {
 	public List<Local> buscar(String busca) {
 		//Implementar busca no Banco de Dados
 		return null;
+	}
+	
+	public String buscar() {
+		
+		String resultado = null;
+		
+		try {	
+			
+			conn = JdbcUtil.createConnection();
+			
+			pstmt = conn.prepareStatement("select * from local");
+			
+			rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				resultado += rs.getString("nome");
+				resultado += "\n";
+			}
+			
+		} catch(Exception e) {
+			throw new RuntimeException(e);
+		} finally {
+			//fecha a conexão
+			JdbcUtil.close(conn, pstmt);
+		}
+		
+		return resultado;
 	}
 }
--- src/main/java/br/ufg/pw/utilitarios/PropertiesManipulador.java
+++ src/main/java/br/ufg/pw/utilitarios/PropertiesManipulador.java
@@ -5,21 +5,26 @@ import java.util.Properties;
 
 public class PropertiesManipulador {
 	
+	//Local do arquivo de configuração
+	private static final String FILE_NAME = "./configs/bd-config.properties";
+	
 	private static Properties props;
 	
 	private PropertiesManipulador() {
 		//Agora é uma classe Singleton. Doidão nenhum consegue instanciar ela! MUAHAHAHAA
 	}
+	
 	public static Properties getProp() {
 		
 		if (props == null) {
-			
 			try {
-				FileInputStream file = new FileInputStream("./configs/bd-config.properties"); //Cria um objeto FileInputStream, passando como parâmetro do construtor o endereço do arquivo de configuração na máquina
-				props.load(file); //Diz para o objeto de propriedade carregar o arquivo de FileInputStream
+				
+				props = new Properties(); //Instancia um objeto Properties
+				FileInputStream file = new FileInputStream( FILE_NAME ); //Cria um objeto FileInputStream, passando como parâmetro do construtor o endereço do arquivo de configuração na máquina
+				props.load( file ); //Diz para o objeto de propriedade carregar o arquivo de FileInputStream
 				
 			} catch(Exception e) {
-				throw new RuntimeException();
+				e.printStackTrace();
 			}
 			
 		}
